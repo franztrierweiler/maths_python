050d7323fdf49633c3394c581f1a56f7f3e4cd14
Franz TRIERWEILER <trierweilerfranz@gmail.com>
2019-12-01 16:56:48 +0000

    Modification du texte de résultat.

---
+++ main.py
@@ -0,0 +1,10 @@
+from math import *
+
+def angle (a,b,c):
+  k=(b**2+c**2-a**2)/(2*b*c)
+  l=(b**2+a**2-c**2)/(2*a*b)
+  m=(c**2+a**2-b**2)/(2*a*c)
+  return [degrees(acos(k)), degrees(acos(l)), degrees(acos(m))]
+
+print ("Résultat du calcul des angles")
+print ("Angle = " + str(angle (5,4,3)) )
