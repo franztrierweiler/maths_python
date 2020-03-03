51f79a4b257959a051d4ebb1838e9ceea4759b9f
franz <trierweilerfranz@gmail.com>
2020-03-03 20:48:14 +0000

    Ajout de l'exercise de Syracuse

--- exercices_02mars.py
+++ exercices_02mars.py
@@ -21,3 +21,28 @@ def compte_espaces(phrase):
     
 la_phrase = "Voici une phrase pour le programmeur Badie"
 print "Il y a " + str(compte_espaces(la_phrase)) + " espaces dans la phrase - " + la_phrase + "-"
+
+def Syracuse(n):
+    resultat = n
+    while (resultat!=1):
+        if (resultat % 2) == 0:
+            resultat = resultat // 2
+        else:
+            resultat = (3 * resultat) + 1
+            
+    return resultat
+    
+def Syracuse2(n):
+    resultat = n
+    compteur = 0
+    while (resultat!=1):
+        compteur = compteur + 1
+        if (resultat % 2) == 0:
+            resultat = resultat // 2
+        else:
+            resultat = (3 * resultat) + 1
+            
+    return compteur
+
+n=10000000000
+print "Syracuse(" + str(n) + ") = " + str(Syracuse(n)) + " avec un temps de vol de " + str(Syracuse2(n))
